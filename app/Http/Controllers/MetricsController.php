922e9efc1421538fb1d7f672e164b8205eef7baf
VOLODYMYR OSTAPIV <VOSTAPIV@N-IX.COM>
2022-08-11 18:10:31 +0300

    Add CollectorRegistry provider

--- app/Http/Controllers/MetricsController.php
+++ app/Http/Controllers/MetricsController.php
@@ -11,6 +11,16 @@ use Prometheus\Storage\InMemory;
 
 class MetricsController extends Controller
 {
+
+    /**
+     * @var CollectorRegistry
+     */
+    protected CollectorRegistry $registry;
+
+    public function __construct(CollectorRegistry $registry){
+        $this->registry = $registry;
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -19,24 +29,22 @@ class MetricsController extends Controller
      */
     public function __invoke(): string
     {
-        $registry = new CollectorRegistry(new InMemory());
-
         //Total
-        $registry
+        $this->registry
         ->getOrRegisterCounter('total', 'models', 'it sets', ['type'])
         ->incBy(MyFirstModel::all()->count(),['count']);
 
         //Recent (newer than subMinutes from now)
         $stamp = Carbon::now()->subMinutes(30)->format("Y-m-d H:i:s");
 
-        $registry
+        $this->registry
             ->getOrRegisterCounter('recent', 'models', $stamp, ['type'])
             ->incBy(MyFirstModel::where('created_at', '>=', $stamp)
                 ->get()
                 ->count(),['count']);
 
         $renderer = new RenderTextFormat();
-        $result = $renderer->render($registry->getMetricFamilySamples());
+        $result = $renderer->render($this->registry->getMetricFamilySamples());
 
         header('Content-type: ' . RenderTextFormat::MIME_TYPE);
         return $result;
--- config/app.php
+++ config/app.php
@@ -194,6 +194,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        //Prometheus metrics
+        App\Providers\PrometheusServiceProvider::class,
 
     ],
 
